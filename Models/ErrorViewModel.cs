f04df0838a4ea50143ec606fca3e94aec9c4ddfe
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-20 09:36:27 -0700

    initialize directories

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcApi.Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
